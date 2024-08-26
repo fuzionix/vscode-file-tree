8c3c7dbc7cae92cf834904efd00016c941f4aac9
DESKTOP-UDBN88M\TinFung <qwertty3566@gmail.com>
2024-08-26 23:24:12 +0800

    ➕ | Add button on explorer context menu

--- src/extension.js
+++ src/extension.js
@@ -4,10 +4,12 @@ const vscode = require('vscode')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const disposable = vscode.commands.registerCommand('file-tree-extractor.copyFileTree', function () {
+	const command = vscode.commands.registerCommand('file-tree-extractor.copyFileTree', function () {
+		const selectedPath = vscode.window.activeTextEditor?.document.fileName || ''
+		console.log(selectedPath)
 		vscode.window.showInformationMessage('Copied file tree to clipboard')
 	})
-	context.subscriptions.push(disposable)
+	context.subscriptions.push(command)
 }
 
 function deactivate() {}
